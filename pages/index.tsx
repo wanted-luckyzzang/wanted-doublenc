2023d0de09beda9429b54627af949e98ff2dd7a8
안병진 <byungjin0120@gmail.com>
2022-02-14 20:53:30 +0900

    init: Initial Settings

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: red;
+`;
+
+const Main = (): JSX.Element => {
+  return <Wrapper>asdasda</Wrapper>;
+};
+export default Main;
