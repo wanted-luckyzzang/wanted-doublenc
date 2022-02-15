f1fc65f9fca241fac60a587e94f38e3436e0a6f1
안병진 <byungjin0120@gmail.com>
2022-02-15 12:10:27 +0900

    chore: Single Quote로 변경

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,6 @@
-import C from "./color";
-import FS from "./fontSize";
-import FW from "./fontWeight";
-import IS from "./iconSize";
+import C from './color';
+import FS from './fontSize';
+import FW from './fontWeight';
+import IS from './iconSize';
 
 export { C, FS, FW, IS };
