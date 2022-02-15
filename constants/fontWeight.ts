855deb2267aa97678963707268079f27359ad81f
안병진 <byungjin0120@gmail.com>
2022-02-15 12:33:35 +0900

    remove: 폴더명 수정하기위해 임시로 지움

--- constants/fontWeight.ts
+++
@@ -1,9 +0,0 @@
-import { PaletteType } from '../types';
-import styleReturn from '../utils';
-
-const FontWeight: PaletteType = {
-  SMALL: styleReturn('fontWeight', '500'),
-  BIG: styleReturn('fontWeight', '600'),
-};
-
-export default FontWeight;
