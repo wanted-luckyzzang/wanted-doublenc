a111a2be8e6bf46903e25a396bacdcce1ebabda0
안병진 <byungjin0120@gmail.com>
2022-02-16 01:39:28 +0900

    design: Card , Category 컴포넌트쪽 스타일 수정

--- src/components/ddang/Card.tsx
+++ src/components/ddang/Card.tsx
@@ -1,57 +1,47 @@
-import { C, FS, FW } from 'src/constants';
-import styled from 'styled-components';
+import { BC, C, FS, FW } from 'src/constants';
 import { Col, Img, Row, Text } from '../common';
 
 const Card = ({ data }: any): JSX.Element => {
-  // console.log('card', data);
   return (
-    <Container>
-      <Row>
-        <div>
-          <Img
-            src={data.imageUrl as string}
-            alt="ddang-icon"
-            sx={{ width: '70px', height: '70px' }}
-          />
-        </div>
-        <Col>
-          <div>
-            <Text sx={{ ...FS.EXPLAIN, ...C.LIGHTGRAY }}>
-              {data.conCategory2.name}
-            </Text>
-          </div>
-          <div>
-            <Text sx={{ ...FS.HEAD }}>{data.name}</Text>
-          </div>
-          <Row>
-            <div>
-              <Text
-                sx={{ ...FS.HEAD, ...C.POINTRED }}
-              >{`${data.discountRate}%`}</Text>
-            </div>
-            <div>
-              <Text sx={{ ...FS.HEAD, ...FW.SMALL }}>
-                {`${data.ncSellingPrice.toLocaleString()}원`}
-              </Text>
-            </div>
-            <div>
-              <Text
-                sx={{
-                  ...FS.EXPLAIN,
-                  ...C.LIGHTGREY,
-                  textDecoration: 'line-through',
-                }}
-              >
-                {`${data.originalPrice.toLocaleString()}원`}
-              </Text>
-            </div>
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+    <Row
+      sx={{
+        ...BC.WHITE,
+        padding: '12px 0 16.5px 21px',
+        borderBottom: `1px solid #F1F3F4`,
+        alignItems: 'center',
+        cursor: 'pointer',
+      }}
+    >
+      <Img
+        src={data.imageUrl as string}
+        alt="ddang-icon"
+        sx={{ width: '70px', height: '70px', marginRight: '17px' }}
+      />
+      <Col>
+        <Text sx={{ ...FS.EXPLAIN, ...C.LIGHTGRAY, marginBottom: '7px' }}>
+          {data.conCategory2.name}
+        </Text>
+        <Text sx={{ ...FS.HEAD, marginBottom: '13px' }}>{data.name}</Text>
+        <Row sx={{ alignItems: 'center' }}>
+          <Text
+            sx={{ ...FS.HEAD, ...C.POINTRED, marginRight: '9px' }}
+          >{`${data.discountRate}%`}</Text>
+          <Text sx={{ ...FS.HEAD, ...FW.SMALL, marginRight: '6px' }}>
+            {`${data.ncSellingPrice.toLocaleString()}원`}
+          </Text>
+          <Text
+            sx={{
+              ...FS.EXPLAIN,
+              ...C.LIGHTGREY,
+              textDecoration: 'line-through',
+            }}
+          >
+            {`${data.originalPrice.toLocaleString()}원`}
+          </Text>
+        </Row>
+      </Col>
+    </Row>
   );
 };
 
 export default Card;
-
-const Container = styled.div``;
